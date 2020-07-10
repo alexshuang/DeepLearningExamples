02f005039226f918587bc7362b23582e798caf9b
Huang Shuang <nikshuang@sia.com>
2020-07-10 04:11:18 +0000

    complete run_resnet50.sh

--- PyTorch/Classification/ConvNets/run_resnet50v1.5.sh
+++ PyTorch/Classification/ConvNets/run_resnet50v1.5.sh
@@ -11,14 +11,30 @@ OUT_DIR=/data/rocm${ROCM_VERSION}_rocblas${ROCBLAS_VERSION}/resnet50v1.5
 rm -rf $OUT_DIR
 mkdir -p $OUT_DIR
 
+#CMD="python3.6 ./main.py $OUT_DIR --arch resnet50 -c fanin --label-smoothing 0.1 --amp --static-loss-scale 128 --data-backend syntetic"
+CMD="python3.6 ./main.py $OUT_DIR --data-backend syntetic --raport-file raport.json -j8 -p 100 --lr 2.048 --warmup 8 --arch resnet50 -c fanin --label-smoothing 0.1 --lr-schedule cosine --mom 0.875 --wd 3.0517578125e-05 --workspace $OUT_DIR --amp --static-loss-scale 128 --epochs 50"
+
+# training
+$CMD --warmup-steps $WARMUP_STEPS --max-steps $STEPS --result-dir $OUT_DIR
+
 # record kernels
 export ROCBLAS_LAYER=2
 export ROCBLAS_LOG_BENCH_PATH=${OUT_DIR}/rocblas_bench.csv
 rm -f ${ROCBLAS_LOG_BENCH_PATH}
+$CMD --max-steps 3 --warmup-steps 0
+rm -f ${OUT_DIR}/*.db ${OUT_DIR}/*.json ${OUT_DIR}/*.txt
 
-echo "pmc: FetchSize L2CacheHit" > input.txt
-/opt/rocm/bin/rocprof -i input.txt --obj-tracking on --timestamp on --stats -o ${OUT_DIR}/kernel_prof.csv \
-python3.6 ./main.py --arch resnet50 -c fanin --label-smoothing 0.1 --amp --static-loss-scale 256 --data-backend "syntetic" --warmup-steps $WARMUP_STEPS --max-steps $STEPS --result-dir $OUT_DIR $OUT_DIR 
-#python3.6 ./main.py $OUT_DIR --data-backend "syntetic" --raport-file raport.json -j8 -p 100 --lr 2.048 --optimizer-batch-size 2048 --warmup 8 --arch resnet50 -c fanin --label-smoothing 0.1 --lr-schedule cosine --mom 0.875 --wd 3.0517578125e-05 -b 256 --static-loss-scale 128 --epochs 1 --warmup-steps $WARMUP_STEPS --max-steps $STEPS --result-dir $OUT_DIR --amp #--workspace $OUT_DIR --data $OUT_DIR
-#python3.6 -m torch.distributed.launch --nproc_per_node 1 ./main.py $OUT_DIR --data-backend syntetic --arch resnet50 -b 32 --epochs 1 --warmup-steps $WARMUP_STEPS --max-steps $STEPS --result-dir $OUT_DIR --amp #--workspace $OUT_DIR --data $OUT_DIR
+# rocblas-bench
+TOOL=/root/rocblas/build/release/clients/staging/rocblas-bench
+if [ ! -e rocblas-bench ]; then
+	ln -s ${TOOL} .
+fi
+unset ROCBLAS_LAYER
+NUM_GEMM_PER_ITER=38
+tail -$NUM_GEMM_PER_ITER $ROCBLAS_LOG_BENCH_PATH > /tmp/rb.csv
+sh /tmp/rb.csv | tee /tmp/rb_res.txt
+sed -E -n '/(^N,|^T,)/p' /tmp/rb_res.txt > $OUT_DIR/rocblas_bench_res.csv
+echo "File $OUT_DIR/rocblas_bench_res.csv is generated."
+
+cp /tmp/rb.csv /data/resnet_rb.csv -v
 
