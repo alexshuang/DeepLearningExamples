71f6f950f133459ba133dce22630571e67f69791
Huang Shuang <nikshuang@sina.com>
2020-07-28 08:44:41 +0000

    update DLRM script

--- PyTorch/Recommendation/DLRM/run_dlrm.sh
+++ PyTorch/Recommendation/DLRM/run_dlrm.sh
@@ -5,11 +5,11 @@ OUT_DIR=${1:-out}
 mkdir -p $OUT_DIR
 rm -rf $OUT_DIR/*
 
-set -e
-
 WARMUP_STEPS=${3:-20}
 TRAIN_STEPS=${2:-100}
 MAX_STEPS=$(expr $WARMUP_STEPS + $TRAIN_STEPS)
+
+dataset_dir=/data/small_terabyte_dataset
 
 #CMD="python3.6 -m dlrm.scripts.main --mode train --synthetic_dataset true --fp16 true --print_freq 1"
 CMD="python3.6 -m dlrm.scripts.main \
@@ -22,6 +22,8 @@ CMD="python3.6 -m dlrm.scripts.main \
   --top_mlp_sizes 1024,1024,512,256,1 \
   --embedding_dim 128 \
   --num_numerical_features 13 \
+  --dataset $dataset_dir \
+  --dataset_type binary \
   --amp \
   --optimized_mlp"
 
@@ -30,12 +32,11 @@ CMD="python3.6 -m dlrm.scripts.main \
 #  --dataset_type binary \
 #  --max_steps 500 \
 
+set -e
 
 # end2end perf
 $CMD --benchmark_warmup_steps ${WARMUP_STEPS} --max_steps ${MAX_STEPS} | tee /tmp/run.log
 sed -n '/^Epoch:\[0/p' /tmp/run.log > ${OUT_DIR}/run_res.csv
-
-exit 0
 
 # record kernels
 export ROCBLAS_LAYER=2
@@ -47,6 +48,13 @@ echo "pmc: FetchSize L2CacheHit" > input.txt
 $CMD --benchmark_warmup_steps 0 --max_steps 1
 rm -f ${OUT_DIR}/*.db ${OUT_DIR}/*.json ${OUT_DIR}/*.txt
 
+# split one iteration
+NUM_GEMM=26
+tail -$NUM_GEMM $ROCBLAS_LOG_BENCH_PATH > /tmp/rb_tail.csv
+cp /tmp/rb_tail.csv $ROCBLAS_LOG_BENCH_PATH
+sed -n '/Cijk_A/p' ${OUT_DIR}/kernel_prof.csv > /tmp/kname.csv
+tail -$NUM_GEMM /tmp/kname.csv > ${OUT_DIR}/kernel_name.csv
+
 # rocblas-bench
 TOOL=/root/rocblas/build/release/clients/staging/rocblas-bench
 if [ ! -e rocblas-bench ]; then
@@ -54,8 +62,6 @@ if [ ! -e rocblas-bench ]; then
 fi
 
 unset ROCBLAS_LAYER
-NUM_GEMM=26
-tail -$NUM_GEMM $ROCBLAS_LOG_BENCH_PATH > /tmp/rb_tail.csv
-sh /tmp/rb_tail.csv | tee /tmp/rb_res.txt
+sh /tmp/rb_tail.csv 2>&1 > /tmp/rb_res.txt
 sed -E -n '/(^N,|^T,)/p' /tmp/rb_res.txt > $OUT_DIR/rocblas_bench_res.csv
 echo "File $OUT_DIR/rocblas_bench_res.csv is generated."
